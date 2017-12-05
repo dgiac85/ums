784580e4f10e40752aba6b48458b9a48ead6f1c9
unknown <dgiac85@gmail.com>
2017-12-05 19:32:20 +0100

    added code for service

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 //Affinche venga considerato come componente dobbiamo mettere un decoratore per decorarlo come component angular
 import {Component} from '@angular/core';
-
+import {UserService} from './user.service';
+ 
 @Component({
     selector:'app-users',
     //template: '<h2>Users</h2>' //Con il back apostrofo si puo andare accapo
@@ -10,4 +11,7 @@ import {Component} from '@angular/core';
 
 export class UsersComponent{
     title = 'Questa è una proprietà relativa agli utenti';
+    constructor () {
+        let service = new UserService();
+    }
 }
